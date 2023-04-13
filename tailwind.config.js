0a47b29d0025b40243a2599ba07fcec199366dae
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-04-14 00:49:04 +0200

    layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      desktop: "821px",
+    },
+    colors: {},
+    fontSize: {
+      menu: [
+        "14px",
+        {
+          lineHeight: "16.45px",
+          fontWeight: "400",
+          letterSpacing: "-0.11px",
+        },
+      ],
+      h1: [
+        "56px",
+        {
+          lineHeight: "64px",
+          fontWeight: "300",
+          letterSpacing: "-1px",
+        },
+      ],
+      text: [
+        "18px",
+        {
+          lineHeight: "28px",
+          fontWeight: "400",
+          letterSpacing: "-0.28px",
+        },
+      ],
+      h2: [
+        "40px",
+        {
+          lineHeight: "64px",
+          fontWeight: "300",
+          letterSpacing: "-0.71px",
+        },
+      ],
+      h3: [
+        "24px",
+        {
+          lineHeight: "28px",
+          fontWeight: "300",
+          letterSpacing: "-0.43px",
+        },
+      ],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
