456e4d163b8c26c3e52c8cf8bcc1a18654080abe
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-04-24 15:50:20 +0200

    articles part finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from "./components/Nav";
 import MainPart from "./components/MainPart";
+import ArticlePart from "./components/ArticlePart";
 
 function App() {
   return (
     <>
       <Nav />
       <MainPart />
+      <ArticlePart />
     </>
   );
 }
